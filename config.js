a40721cb1886e28d50cf1afd852ff504ec5e7574
Dias <dias@quantified.sg>
2014-12-19 14:48:36 +0800

    Modified to Bitstamp Structure

---
+++ config.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var _ = require('lodash');
+
+exports.NAME = 'Itbit';
+exports.SUPPORTED_MODULES = ['ticker','trader'];
+exports.API_ENDPOINT = 'https://api.itbit.com/v1/markets/';
+
+exports.SATOSHI_FACTOR = 1e8;
+exports.FUDGE_FACTOR = 1.05;
+
+exports.config = function config(localConfig) {
+  if (localConfig) _.merge(exports, localConfig);
+};
--- test/ticker.js
+++ test/ticker.js
@@ -2,9 +2,12 @@
 
 'use strict';
 
-var should = require('chai').should();
+var should        = require('chai').should();
 
-var testedCurrencies = ['USD', 'SGD'];
+var pluginConfig  = require('../config');
+
+
+var testedCurrencies = ['USD','EUR'];
 
 
 // Checks structure and values of object returned by `.balance()`
@@ -36,112 +39,115 @@ function checkCurrency(results, currency) {
 }
 
 
-var tickerPlugin = require('../ticker');
-describe(tickerPlugin.NAME + ' Ticker', function() {
+// only test if ticker is declared to be working
+if (pluginConfig.SUPPORTED_MODULES.indexOf('ticker') !== -1) {
+  describe(pluginConfig.NAME + ' Ticker', function() {
+    var tickerPlugin = require('../index');
 
-  // // NOTE: should be uncommented and adjusted when rate limiting is in place
-  // afterEach(function(done) {
-  //   setTimeout(done, 1000);
-  // });
+    // // NOTE: should be uncommented and adjusted when rate limiting is in place
+    // afterEach(function(done) {
+    //   setTimeout(done, 1000);
+    // });
 
-  // NOTE: MAX timeout for each test
-  this.timeout(2000);
+    // NOTE: MAX timeout for each test
+    this.timeout(3000);
 
-  // single supported currency fetch (as string)
-  it('should read ticker in \'' + testedCurrencies[0] + '\'', function(done) {
-    tickerPlugin.ticker(testedCurrencies[0], function(err, results) {
-      should.not.exist(err, 'There should be no error');
-      should.exist(results);
-
-      checkCurrency(results, testedCurrencies[0]);
-
-      done();
-    });
-  });
-
-
-  var tmpCurrency = testedCurrencies.length >= 2 ?
-    testedCurrencies[1] :
-    testedCurrencies[0];
-
-  // single supported currency fetch (as array)
-  it('should read ticker in [' + tmpCurrency + ']', function(done) {
-    tickerPlugin.ticker([tmpCurrency], function(err, results) {
-      should.not.exist(err, 'There should be no error');
-      should.exist(results);
-
-      checkCurrency(results, tmpCurrency);
-
-      done();
-    });
-  });
-
-
-  // will be used in following tests
-  var unsupportedCurrency = 'ABC';
-
-  // single *not* supported currency fetch
-  it('should fail to read ticker in ' + unsupportedCurrency, function(done) {
-    tickerPlugin.ticker([unsupportedCurrency], function(err, results) {
-      should.exist(err);
-      should.not.exist(results);
-
-      done();
-    });
-  });
-
-
-  // only make sense if there's more than one supported currency
-  if (testedCurrencies.length >= 2) {
-    // multiple supported currencies fetch
-    it('should read ticker in ' + testedCurrencies.join(', '), function(done) {
-
-      tickerPlugin.ticker(testedCurrencies, function(err, results) {
+    // single supported currency fetch (as string)
+    it('should read ticker in \'' + testedCurrencies[0] + '\'', function(done) {
+      tickerPlugin.ticker(testedCurrencies[0], function(err, results) {
         should.not.exist(err, 'There should be no error');
         should.exist(results);
 
-        for(var i in testedCurrencies)
-          checkCurrency(results, testedCurrencies[i]);
+        checkCurrency(results, testedCurrencies[0]);
 
         done();
       });
     });
 
 
-    // multiple supported and not supported currencies
-    var mix2Currencies = [unsupportedCurrency].concat(testedCurrencies);
-    it('should fail to read ticker in ' + mix2Currencies.join(', '), function(done) {
-      tickerPlugin.ticker(mix2Currencies, function(err, results) {
+    var tmpCurrency = testedCurrencies.length >= 2 ?
+      testedCurrencies[1] :
+      testedCurrencies[0];
+
+    // single supported currency fetch (as array)
+    it('should read ticker in [' + tmpCurrency + ']', function(done) {
+      tickerPlugin.ticker([tmpCurrency], function(err, results) {
+        should.not.exist(err, 'There should be no error');
+        should.exist(results);
+
+        checkCurrency(results, tmpCurrency);
+
+        done();
+      });
+    });
+
+
+    // will be used in following tests
+    var unsupportedCurrency = 'ABC';
+
+    // single *not* supported currency fetch
+    it('should fail to read ticker in ' + unsupportedCurrency, function(done) {
+      tickerPlugin.ticker([unsupportedCurrency], function(err, results) {
         should.exist(err);
         should.not.exist(results);
 
         done();
       });
     });
-  }
 
 
-  // mix of supported AND not supported currencies
-  var mixCurrencies = [testedCurrencies[0], unsupportedCurrency];
-  it('should fail to read ticker with ' + mixCurrencies.join(', '), function(done) {
-    tickerPlugin.ticker(mixCurrencies, function(err, results) {
-      should.exist(err);
-      should.not.exist(results);
+    // only make sense if there's more than one supported currency
+    if (testedCurrencies.length >= 2) {
+      // multiple supported currencies fetch
+      it('should read ticker in ' + testedCurrencies.join(', '), function(done) {
 
-      done();
+        tickerPlugin.ticker(testedCurrencies, function(err, results) {
+          should.not.exist(err, 'There should be no error');
+          should.exist(results);
+
+          for(var i in testedCurrencies)
+            checkCurrency(results, testedCurrencies[i]);
+
+          done();
+        });
+      });
+
+
+      // multiple supported and not supported currencies
+      var mix2Currencies = [unsupportedCurrency].concat(testedCurrencies);
+      it('should fail to read ticker in ' + mix2Currencies.join(', '), function(done) {
+        tickerPlugin.ticker(mix2Currencies, function(err, results) {
+          should.exist(err);
+          should.not.exist(results);
+
+          done();
+        });
+      });
+    }
+
+
+    // mix of supported AND not supported currencies
+    var mixCurrencies = [testedCurrencies[0], unsupportedCurrency];
+    it('should fail to read ticker with ' + mixCurrencies.join(', '), function(done) {
+      tickerPlugin.ticker(mixCurrencies, function(err, results) {
+        should.exist(err);
+        should.not.exist(results);
+
+        done();
+      });
     });
-  });
 
 
-  // ticker request with empty array
-  var emptyArray = [];
-  it('should fail to read ticker with empty array', function(done) {
-    tickerPlugin.ticker(emptyArray, function(err, results) {
-      should.exist(err);
-      should.not.exist(results);
+    // ticker request with empty array
+    var emptyArray = [];
+    it('should fail to read ticker with empty array', function(done) {
+      tickerPlugin.ticker(emptyArray, function(err, results) {
+        should.exist(err);
+        should.not.exist(results);
 
-      done();
+        done();
+      });
     });
-  });
 
-});
+  });
+}
